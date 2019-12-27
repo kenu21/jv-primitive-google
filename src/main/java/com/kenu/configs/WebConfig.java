44e840f9703b04f9a6fa14c9e58a797dcf979dcb
Iurii <kenu21@gmail.com>
2019-12-27 20:47:10 +0200

    Added index controller

---
+++ src/main/java/com/kenu/configs/WebConfig.java
@@ -0,0 +1,27 @@
+package com.kenu.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScans(value = {
+        @ComponentScan("com.kenu.controllers")
+})
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public InternalResourceViewResolver resolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/WEB-INF/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
---
+++ src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,19 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Index</title>
+</head>
+<body>
+<form action="${pageContext.request.contextPath}/index/abra" method="post">
+    <div class="container">
+        <label for="q"><b>Query</b></label>
+        <input type="text" placeholder="Enter uri" name="q" required>
+
+        <label for="level"><b>Level</b></label>
+        <input type="text" placeholder="Enter level" name="level" required>
+
+        <button type="submit" class="registerbtn">Index</button>
+    </div>
+</form>
+</body>
+</html>
